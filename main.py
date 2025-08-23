882be9c8490d02d57bedbb70d94954722b586b59
Luis Guilherme Pelinson Giani <lggiani@gmail.com>
2025-08-23 17:39:41 -0300

    Criação da página

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import flet as ft
 
 def main(page: ft.Page):
-    
+    page.bgcolor = 'gray'
+    page.window.resizable = False
+    page.window.width = 730
+    page.window.height = 920
+    page.title = 'Calculadora'
+    page.window.always_on_top = True
+
+ft.app(target=main)
