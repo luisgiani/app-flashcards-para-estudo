d9d7ace38a0629e432d28f95acaf1810d4e4721b
Luis Guilherme Pelinson Giani <lggiani@gmail.com>
2025-10-14 21:45:42 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 import flet as ft
 
 def main(page: ft.Page):
-    page.bgcolor = 'gray'
-    page.window.resizable = False
-    page.window.width = 730
-    page.window.height = 920
-    page.title = 'Flashcards'
-    page.window.always_on_top = True
+    page.title = "Projetinho Flashcards"
+    page.window.width = 830
+    page.window.height = 800
+    page.bgcolor = "gray"
 
+    texto = ft.Row(controls=[
+        ft.Text(value="Qual é a cor de plano de fundo?", color="white", size= 20, text_align='center')],
+        alignment='center'
+    )
 
+    digitar = ft.Row(controls=[
+        ft.TextField(label="Digite aqui a sua resposta:", text_size=20)],
+        alignment='center'
+        )
+    
+    btn = ft.Row(controls= [
+        ft.ElevatedButton(text="Responder")],
+        alignment='center' 
+        )
 
-ft.app(target=main)
+    page.add(texto,digitar, btn)
+
+ft.app(main)
